2149f9564bae9e3323757ee4fa2d9c98f3c2a197
Larry Volz <imaginologist@gmail.com>
2020-12-12 13:35:30 -0500

    finished 1st 2

--- convert.py
+++ convert.py
@@ -1,6 +1,9 @@
 def convert_temp(unit_in, unit_out, temp):
     """Convert farenheit <-> celsius and return results.
 
+    C = (f-32)/1.8
+    F = (1.8*c)+32
+
     - unit_in: either "f" or "c" 
     - unit_out: either "f" or "c"
     - temp: temperature (in f or c, depending on unit_in)
@@ -14,8 +17,16 @@ def convert_temp(unit_in, unit_out, temp):
       convert_temp("c", "f", 0)  =>  32.0
       convert_temp("f", "c", 212) => 100.0
     """
-
+    #todo: invalid number check
     # YOUR CODE HERE
+    if unit_in == unit_out and (temp is int or float):
+          return unit_in
+    elif unit_in.lower() == "c" and unit_out.lower() =="f" and (temp is int or float):
+        return (1.8*temp)+32
+    elif unit_in.lower() =="f" and unit_out.lower() =="c" and (temp is int or float):
+        return (temp-32)/1.8
+    else:
+        return "Invalid unit [UNIT_IN]"
 
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
