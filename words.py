080f0443180b444f8c319f585364c1971dbeefab
Larry Volz <imaginologist@gmail.com>
2020-12-12 14:11:58 -0500

    finished with word assignment

---
+++ words.py
@@ -0,0 +1,14 @@
+def print_upper_words(words, must_start_with):
+    """ For a list of words, print out each word on a separate line, 
+    but in all uppercase. """
+
+
+    for word in words:
+        for letter in must_start_with:
+            if word[0] == letter:
+                print(word.upper())
+
+
+# this should print "HELLO", "HEY", "YO", and "YES"
+
+print_upper_words(["hello", "hey", "goodbye", "yo", "yes"],  must_start_with={"h", "y"})
